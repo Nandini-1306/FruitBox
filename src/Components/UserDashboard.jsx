bd68fc440be5e40e9696df407df919bdea4386a1
Nandini-1306 <145739888+Nandini-1306@users.noreply.github.com>
2024-09-20 22:52:12 +0530

    Add files via upload

---
+++ src/Components/UserDashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductList from './productList.jsx'
+import DashboardNavbar from './DashboardNavbar.jsx'
+
+function UserDashboard() {
+  return (
+    <>
+        <DashboardNavbar/>
+        <ProductList/>
+    </>
+  )
+}
+
+export default UserDashboard
